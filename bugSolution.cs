ccf92ac1ed4362bb4094335720a85a0a0ab76175
BugHunter-X <askzenai@gmail.com>
2024-12-16 23:26:05 +0100

    Add bugSolution.cs file

---
+++ bugSolution.cs
@@ -0,0 +1,8 @@
+public class MyClass {
+    public int MyProperty { get; set; } = 0; // Initialize the property
+
+    public void MyMethod() {
+        // Accessing a property that has been initialized
+        int value = MyProperty * 2; 
+    }
+}
