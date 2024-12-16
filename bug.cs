399ce51200505f6951e401cbaec58703ca1613b5
BugHunter-X <askzenai@gmail.com>
2024-12-16 23:26:04 +0100

    Add bug.cs file

---
+++ bug.cs
@@ -0,0 +1,8 @@
+public class MyClass {
+    public int MyProperty { get; set; }
+
+    public void MyMethod() {
+        // Accessing a property that hasn't been initialized
+        int value = MyProperty * 2; // potential NullReferenceException
+    }
+}
